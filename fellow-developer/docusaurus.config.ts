7533f9fccc9642b7a42ea130c969416b5a9a3bbd
Pär Dahlman <par.dahlman@fellowdeveloper.se>
2024-10-17 21:50:10 +0200

    Change title and remove logo

--- fellow-developer/docusaurus.config.ts
+++ fellow-developer/docusaurus.config.ts
@@ -61,11 +61,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
+      title: 'Fellow Developer',
       items: [
         {to: '/about', label: 'About', position: 'right'},
         {
