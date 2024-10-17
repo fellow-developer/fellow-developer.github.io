ac2190220774430221bc9882828ad9b4afbba1ed
Pär Dahlman <par.dahlman@fellowdeveloper.se>
2024-10-17 21:11:28 +0200

    Basic footer with socials

--- fellow-developer/docusaurus.config.ts
+++ fellow-developer/docusaurus.config.ts
@@ -85,46 +85,19 @@ const config: Config = {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
+          label: 'GitHub',
+          href: 'https://github.com/fellow-developer',
         },
         {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
+          label: 'LinkedIn',
+          href: 'https://linkedin.com/company/fellow-developer',
         },
         {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
+          label: 'X',
+          href: 'https://x.com/fellowdeveloper',
+        }
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `${new Date().getFullYear()} Fellow Developer`,
     },
     prism: {
       theme: prismThemes.github,
