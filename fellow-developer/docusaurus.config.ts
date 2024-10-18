ca63f2182c63fa3c6e0c295aa6fd5359f2795cd5
Pär Dahlman <par.dahlman@fellowdeveloper.se>
2024-10-18 19:36:15 +0200

    Use dark mode by default

--- fellow-developer/docusaurus.config.ts
+++ fellow-developer/docusaurus.config.ts
@@ -60,6 +60,10 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      defaultMode: 'dark',
+      respectPrefersColorScheme: false,
+    },
     navbar: {
       title: 'Fellow Developer',
       items: [
